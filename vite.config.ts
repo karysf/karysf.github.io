f282cbde630b91e20b3c3d5ef55d194b3b949ba2
karysf <46572255+karysf@users.noreply.github.com>
2024-03-16 22:32:28 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react(), svgr()],
-    base: "/pipefitting/",
+    base: "./",
   };
 
   return config;
